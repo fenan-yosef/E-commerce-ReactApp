b28bad205cd5bc9e0c77f73db63350f10e25bd7c
pal-oe <fenanyosef@gmail.com>
2024-07-02 19:40:45 +0300

    added card

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+
+import { ShoppingCart } from "@material-ui/icons";
+
+const Navbar = () => {
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        className={classes.appBar}
+        color="inherit"
+      ></AppBar>
+    </>
+  );
+};
+
+export default Navbar;
