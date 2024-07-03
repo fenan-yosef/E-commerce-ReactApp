a0e3240bc62b32916ca518ba420ceb98649416e6
pal-oe <fenanyosef@gmail.com>
2024-07-03 18:46:58 +0300

    adding text field

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+import CustomTextField from "./CustomTextField";
 
 const AddressForm = () => {
-  return <div>AddressForm</div>;
+  const methods = useForm();
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping Address
+      </Typography>
+      <FormProvider {...methods}>
+        <form onSubmit="">
+          <Grid container spacing={3}>
+            <CustomTextField required name="firstName" label="FirstName" />
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
 };
 
 export default AddressForm;
